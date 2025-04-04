934f68e56967ba0e625f07bace649a40a2912fef
Kaplin Artyom <kaplinstu1@gmail.com>
2025-04-04 17:42:50 +0300

    add arg parser, and const to base file

--- akinator/main.cpp
+++ akinator/main.cpp
@@ -6,13 +6,24 @@
 #include "akinator.h"
 #include "color.h"
 #include "logger.h"
+#include "arg_parser.h"
 
-int main() // TODO const
+int main(int argc, const char* argv[])
 {
-    LoggerInit(LOGL_DEBUG, "lib/logger/logfile.log", DEFAULT_MODE); //TODO logget(output)
+    const char *log_file = "lib/logger/logfile.log";
+    LoggerInit(LOGL_DEBUG, log_file, DEFAULT_MODE);
+
+    ArgOption options[] = { {"-i",  "--input", true, nullptr, false},
+                            {"-o", "--output", true, nullptr, false} };
+
+    if (ParseArguments(argc, argv, options, sizeof(options) / sizeof(options[0])) != PARSE_OK)
+    {
+        fprintf(stderr, "ParseArg error\n");
+        return 1;
+    }
 
     BTree *Root = nullptr;
-    const char *name_base = "akinator/base.txt"; //TODO arg
+    const char *name_base = options[INPUT].argument;
 
     MenuGuessing(&Root, name_base);
     TreeDumpDot(Root);
